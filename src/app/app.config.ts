bf41dd452e6874ff9f1a981f8538e9a4d61859c2
Bradley Biehl <bradley.biehl@gmail.com>
2024-12-22 13:45:17 -0600

    [setup] feat: add Firebase configuration and update app providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,28 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: process.env['FIREBASE_PROJECT_ID'],
+        appId: process.env['FIREBASE_APP_ID'],
+        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+        apiKey: process.env['FIREBASE_API_KEY'],
+        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
 };
