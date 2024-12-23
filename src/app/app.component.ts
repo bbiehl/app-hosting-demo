045eb4939446e21a1da59b4980175325aa5bd85f
Bradley Biehl <bradley.biehl@gmail.com>
2024-12-23 10:34:29 -0600

    [setup] feat: integrate Firestore data retrieval and display in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [AsyncPipe, JsonPipe, RouterOutlet],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'app-hosting-demo';
+export class AppComponent implements OnInit {
+    private firestore = inject(Firestore);
+    private collectionRef = collection(this.firestore, 'demo');
+    items$ = collectionData(this.collectionRef, { idField: 'id' });
+    title = 'app-hosting-demo';
+
+    ngOnInit() {
+        console.log(process.env['NODE_ENV']);
+    }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,35 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: process.env['FIREBASE_PROJECT_ID'],
-        appId: process.env['FIREBASE_APP_ID'],
-        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-        apiKey: process.env['FIREBASE_API_KEY'],
-        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-      })
-    ),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
-  ],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideClientHydration(),
+        provideFirebaseApp(() =>
+            initializeApp({
+                apiKey: 'AIzaSyD3I-WXQ8y9GJLDrAkoXR8vPoUrZg0DDp0',
+                authDomain: 'fir-app-hosting-e787b.firebaseapp.com',
+                projectId: 'fir-app-hosting-e787b',
+                storageBucket: 'fir-app-hosting-e787b.firebasestorage.app',
+                messagingSenderId: '912650349399',
+                appId: '1:912650349399:web:af06ddfafabfa05f92ac12',
+                // projectId: process.env['PROJECT_ID'],
+                // appId: process.env['APP_ID'],
+                // storageBucket: process.env['STORAGE_BUCKET'],
+                // apiKey: process.env['API_KEY'],
+                // authDomain: process.env['AUTH_DOMAIN'],
+                // messagingSenderId: process.env['MESSAGING_SENDER_ID'],
+            })
+        ),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideStorage(() => getStorage()),
+    ],
 };
