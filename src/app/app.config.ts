8ca4f051eabbd4ae67cae09fa6a54898eafdb82d
Bradley Biehl <bradley.biehl@gmail.com>
2024-12-26 07:30:44 -0700

    [setup] feat: update Firebase configuration to use environment variables for sensitive data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,18 +14,12 @@ export const appConfig: ApplicationConfig = {
         provideClientHydration(),
         provideFirebaseApp(() =>
             initializeApp({
-                apiKey: 'AIzaSyD3I-WXQ8y9GJLDrAkoXR8vPoUrZg0DDp0',
-                authDomain: 'fir-app-hosting-e787b.firebaseapp.com',
-                projectId: 'fir-app-hosting-e787b',
-                storageBucket: 'fir-app-hosting-e787b.firebasestorage.app',
-                messagingSenderId: '912650349399',
-                appId: '1:912650349399:web:af06ddfafabfa05f92ac12',
-                // projectId: process.env['PROJECT_ID'],
-                // appId: process.env['APP_ID'],
-                // storageBucket: process.env['STORAGE_BUCKET'],
-                // apiKey: process.env['API_KEY'],
-                // authDomain: process.env['AUTH_DOMAIN'],
-                // messagingSenderId: process.env['MESSAGING_SENDER_ID'],
+                projectId: process.env['PROJECT_ID'],
+                appId: process.env['APP_ID'],
+                storageBucket: process.env['STORAGE_BUCKET'],
+                apiKey: process.env['API_KEY'],
+                authDomain: process.env['AUTH_DOMAIN'],
+                messagingSenderId: process.env['MESSAGING_SENDER_ID'],
             })
         ),
         provideAuth(() => {
